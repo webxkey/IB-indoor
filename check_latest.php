bbaa0c764eb83384c4805fdf1a5c256087899ef9
Saman <mohamedshaman003@gmail.com>
2026-06-08 09:32:43 +0530

    delete test file

--- check_latest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$users = \App\Models\User::orderBy('id', 'desc')->take(5)->get();
-foreach ($users as $u) {
-    echo "ID: {$u->id} | Email: {$u->email} | Role: {$u->role} | Pass: " . substr($u->password, 0, 10) . "...\n";
-}
